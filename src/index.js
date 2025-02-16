d0d5216d72437714cd6918c8d09ee6be3f1060c0
michaelga0 <73146524+michaelga0@users.noreply.github.com>
2025-02-16 17:15:34 -0500

    Initial skeleton

---
+++ src/index.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const fs = require('fs')
+const path = require('path')
+const { Client, Collection, GatewayIntentBits, Routes } = require('discord.js')
+const { REST } = require('@discordjs/rest')
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+client.commands = new Collection()
+const commands = []
+const commandsPath = path.join(__dirname, 'commands')
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+  const command = require(path.join(commandsPath, file))
+  client.commands.set(command.data.name, command)
+  commands.push(command.data.toJSON())
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN)
+
+const guildId = 'GUILD_ID';
+client.once('ready', async () => {
+  console.log('Bot is online')
+  try {
+    await rest.put(Routes.applicationCommands(client.user.id, guildId), { body: commands })
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isChatInputCommand()) return
+  const command = client.commands.get(interaction.commandName)
+  if (!command) return
+  try {
+    await command.execute(interaction)
+  } catch (error) {
+    console.error(error)
+    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+  }
+})
+
+client.login(process.env.BOT_TOKEN)
