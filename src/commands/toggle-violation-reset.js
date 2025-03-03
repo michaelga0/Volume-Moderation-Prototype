28d55067117b0dd0bb55b21e39e27baab09ab536
michaelga0 <73146524+michaelga0@users.noreply.github.com>
2025-03-02 23:32:57 -0500

    Added toggle violation reset command

---
+++ src/commands/toggle-violation-reset.js
@@ -0,0 +1,46 @@
+const { SlashCommandBuilder, PermissionFlagsBits, MessageFlags } = require('discord.js')
+const { ServerSettings } = require('../database/init-db')
+const { writeLog } = require('../utils/logger')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('toggleviolationreset')
+    .setDescription('Enable or disable the violation reset feature for this server.')
+    .addBooleanOption(option =>
+      option
+        .setName('enabled')
+        .setDescription('True to enable violation resets, false to disable them.')
+        .setRequired(true)
+    )
+    // Requires Manage Guild permission for the user invoking the command.
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+  async execute(interaction) {
+    const guildId = interaction.guildId
+    const enabled = interaction.options.getBoolean('enabled')
+
+    try {
+      let serverSettings = await ServerSettings.findOne({ where: { guild_id: guildId } })
+      if (!serverSettings) {
+        serverSettings = await ServerSettings.create({ guild_id: guildId })
+      }
+
+      serverSettings.violation_reset_enabled = enabled
+      await serverSettings.save()
+
+      await interaction.reply({
+        content: `Violation reset has been **${enabled ? 'enabled' : 'disabled'}**.`,
+        flags: MessageFlags.Ephemeral
+      })
+
+      writeLog(
+        `Violation reset set to ${enabled} for guild ${guildId} by user ${interaction.user.tag}`
+      )
+    } catch (error) {
+      writeLog(`Failed to toggle violation reset for guild ${guildId}: ${error}`)
+      await interaction.reply({
+        content: 'An error occurred while toggling violation reset. Please try again later.',
+        flags: MessageFlags.Ephemeral
+      })
+    }
+  }
+}
